6fd9b9170822db7478c3916a8871d873d9bc96f2
rawaanalshayji <66734178+rawaanalshayji@users.noreply.github.com>
2020-06-20 21:24:34 +0300

    done
    done

--- Classwork3iOS/Classwork3iOS/ViewController.swift
+++ Classwork3iOS/Classwork3iOS/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
         
         
         // MARK: -  قم باستدعاء الداله هنا
-        
+        addName(name: name, helloArray: helloLangauge, flagArray: flagLanguage)
         
         // MARK: -  النهايه⚠️⚠️لا تقم بغير تحت هذا السطر
         
@@ -48,7 +48,11 @@ class ViewController: UIViewController {
     
     // MARK: -  قم بكتابة الداله هنا
     
-    
+    func addName(name: String, helloArray: [String], flagArray: [String]){
+        for i in 0..<helloArray.count{
+            greetingArray[i] = "\(helloArray[i]) \(name) \(flagArray[i])"
+        }
+    }
     
     // MARK: -   النهايه⚠️⚠️ لا تقوم بغير تحت هذا السطر
     
